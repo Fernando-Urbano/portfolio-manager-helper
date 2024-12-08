5babe34a071fbfb9b435ff228573899a154a6d4d
Fernando Rocha Correa Urbano <fernandorochacorreaurbano@fernandourbano-macbook.local>
2024-12-08 15:47:32 -0600

    package setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="your_package_name",
+    version="0.1.0",
+    description="A brief description of your package",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    install_requires=[
+        "pandas==2.2.3",
+        "numpy==2.1.1",
+        "matplotlib==3.9.2",
+        "scipy==1.14.1",
+        "yfinance==0.2.43",
+        "datetime==3.0.3",
+        "statsmodels==0.14.3",
+        "scikit-learn==1.4.2",
+        "seaborn==0.13.2",
+        "openpyxl==3.1.3",
+        "ipywidgets==8.1.5",
+        "arch==7.1.0",
+        "setuptools==57.5.0"
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

baf21c27298cbc05666d1b0bb18e8389003b2d24
Thomas Lesniak <thomaslesniak@proton.me>
2024-12-23 22:15:50 -0600

    run black formatting on portfolio_management

--- portfolio_management/statistics.py
+++ portfolio_management/statistics.py
@@ -17,7 +17,6 @@ pd.options.display.float_format = "{:,.4f}".format
 warnings.filterwarnings("ignore")
 
 
-
 def calc_negative_pct(
     returns: Union[pd.DataFrame, pd.Series, list],
     calc_positive: bool = False,
@@ -47,35 +46,37 @@ def calc_negative_pct(
         returns_list = returns[:]
         returns = pd.DataFrame({})
         for series in returns_list:
-            returns = returns.merge(series, right_index=True, left_index=True, how='outer')
+            returns = returns.merge(
+                series, right_index=True, left_index=True, how="outer"
+            )
 
-    if 'date' in returns.columns.str.lower():
-        returns = returns.rename({'Date': 'date'}, axis=1)
-        returns = returns.set_index('date')
-        
-    returns.index.name = 'date'
+    if "date" in returns.columns.str.lower():
+        returns = returns.rename({"Date": "date"}, axis=1)
+        returns = returns.set_index("date")
+
+    returns.index.name = "date"
 
     if isinstance(returns, pd.Series):
         returns = returns.to_frame()
     returns = returns.apply(lambda x: x.astype(float))
     prev_len_index = returns.apply(lambda x: len(x))
-    returns  =returns.dropna(axis=0)
+    returns = returns.dropna(axis=0)
     new_len_index = returns.apply(lambda x: len(x))
     if not (prev_len_index == new_len_index).all():
-        print('Some columns had NaN values and were dropped')
+        print("Some columns had NaN values and were dropped")
     if calc_positive:
         returns = returns.applymap(lambda x: 1 if x > 0 else 0)
     else:
         returns = returns.applymap(lambda x: 1 if x < 0 else 0)
 
-    negative_statistics = (
-        returns
-        .agg(['mean', 'count', 'sum'])
-        .set_axis(['% Negative Returns', 'Nº Returns', 'Nº Negative Returns'], axis=0)
+    negative_statistics = returns.agg(["mean", "count", "sum"]).set_axis(
+        ["% Negative Returns", "Nº Returns", "Nº Negative Returns"], axis=0
     )
 
     if calc_positive:
-        negative_statistics = negative_statistics.rename(lambda i: i.replace('Negative', 'Positive'), axis=0)
+        negative_statistics = negative_statistics.rename(
+            lambda i: i.replace("Negative", "Positive"), axis=0
+        )
 
     return _filter_columns_and_indexes(
         negative_statistics,
@@ -83,14 +84,14 @@ def calc_negative_pct(
         drop_columns=drop_columns,
         keep_indexes=keep_indexes,
         drop_indexes=drop_indexes,
-        drop_before_keep=drop_before_keep
+        drop_before_keep=drop_before_keep,
     )
 
 
 def get_best_and_worst(
     summary_statistics: pd.DataFrame,
-    stat: str = 'Annualized Sharpe',
-    return_df: bool = True
+    stat: str = "Annualized Sharpe",
+    return_df: bool = True,
 ):
     """
     Identifies the best and worst assets based on a specified statistic.
@@ -106,23 +107,30 @@ def get_best_and_worst(
     summary_statistics = summary_statistics.copy()
 
     if len(summary_statistics.index) < 2:
-        raise Exception('"summary_statistics" must have at least two lines in order to do comparison')
+        raise Exception(
+            '"summary_statistics" must have at least two lines in order to do comparison'
+        )
 
     if stat not in summary_statistics.columns:
         raise Exception(f'{stat} not in "summary_statistics"')
-    summary_statistics.rename(columns=lambda c: c.replace(' ', '').lower())
+    summary_statistics.rename(columns=lambda c: c.replace(" ", "").lower())
     best_stat = summary_statistics[stat].max()
     worst_stat = summary_statistics[stat].min()
-    asset_best_stat = summary_statistics.loc[lambda df: df[stat] == df[stat].max()].index[0]
-    asset_worst_stat = summary_statistics.loc[lambda df: df[stat] == df[stat].min()].index[0]
-    print(f'The asset with the highest {stat} is {asset_best_stat}: {best_stat:.5f}')
-    print(f'The asset with the lowest {stat} is {asset_worst_stat}: {worst_stat:.5f}')
+    asset_best_stat = summary_statistics.loc[
+        lambda df: df[stat] == df[stat].max()
+    ].index[0]
+    asset_worst_stat = summary_statistics.loc[
+        lambda df: df[stat] == df[stat].min()
+    ].index[0]
+    print(f"The asset with the highest {stat} is {asset_best_stat}: {best_stat:.5f}")
+    print(f"The asset with the lowest {stat} is {asset_worst_stat}: {worst_stat:.5f}")
     if return_df:
-        return pd.concat([
-            summary_statistics.loc[lambda df: df.index == asset_best_stat],
-            summary_statistics.loc[lambda df: df.index == asset_worst_stat]
-        ])
-    
+        return pd.concat(
+            [
+                summary_statistics.loc[lambda df: df.index == asset_best_stat],
+                summary_statistics.loc[lambda df: df.index == asset_worst_stat],
+            ]
+        )
 
 
 def calc_correlations(
@@ -134,7 +142,7 @@ def calc_correlations(
     drop_columns: Union[list, str] = None,
     keep_indexes: Union[list, str] = None,
     drop_indexes: Union[list, str] = None,
-    drop_before_keep: bool = False
+    drop_before_keep: bool = False,
 ):
     """
     Calculates the correlation matrix of the provided returns and optionally prints or visualizes it.
@@ -155,16 +163,16 @@ def calc_correlations(
     """
     returns = returns.copy()
 
-    if 'date' in returns.columns.str.lower():
-        returns = returns.rename({'Date': 'date'}, axis=1)
-        returns = returns.set_index('date')
-    returns.index.name = 'date'
+    if "date" in returns.columns.str.lower():
+        returns = returns.rename({"Date": "date"}, axis=1)
+        returns = returns.set_index("date")
+    returns.index.name = "date"
 
     correlation_matrix = returns.corr()
     if return_heatmap:
         fig, ax = plt.subplots(figsize=(matrix_size * 1.5, matrix_size))
         heatmap = sns.heatmap(
-            correlation_matrix, 
+            correlation_matrix,
             xticklabels=correlation_matrix.columns,
             yticklabels=correlation_matrix.columns,
             annot=True,
@@ -172,18 +180,21 @@ def calc_correlations(
 
     if print_highest_lowest:
         highest_lowest_corr = (
-            correlation_matrix
-            .unstack()
+            correlation_matrix.unstack()
             .sort_values()
             .reset_index()
-            .set_axis(['asset_1', 'asset_2', 'corr'], axis=1)
+            .set_axis(["asset_1", "asset_2", "corr"], axis=1)
             .loc[lambda df: df.asset_1 != df.asset_2]
         )
-        highest_corr = highest_lowest_corr.iloc[lambda df: len(df)-1, :]
+        highest_corr = highest_lowest_corr.iloc[lambda df: len(df) - 1, :]
         lowest_corr = highest_lowest_corr.iloc[0, :]
-        print(f'The highest correlation ({highest_corr["corr"]:.2%}) is between {highest_corr.asset_1} and {highest_corr.asset_2}')
-        print(f'The lowest correlation ({lowest_corr["corr"]:.2%}) is between {lowest_corr.asset_1} and {lowest_corr.asset_2}')
-    
+        print(
+            f'The highest correlation ({highest_corr["corr"]:.2%}) is between {highest_corr.asset_1} and {highest_corr.asset_2}'
+        )
+        print(
+            f'The lowest correlation ({lowest_corr["corr"]:.2%}) is between {lowest_corr.asset_1} and {lowest_corr.asset_2}'
+        )
+
     if return_heatmap:
         return heatmap
     else:
@@ -193,6 +204,5 @@ def calc_correlations(
             drop_columns=drop_columns,
             keep_indexes=keep_indexes,
             drop_indexes=drop_indexes,
-            drop_before_keep=drop_before_keep
+            drop_before_keep=drop_before_keep,
         )
-    
